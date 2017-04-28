cb5438cd84fed59a21d7390ee678bb8902be3e8b
Owen Cummings <owenpatrickcummings@gmail.com>
2017-04-28 11:20:35 -0400

    Formatting + blank js file
    Formatting + blank js file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # owencummings.github.io
 Github Website
+
+Working through a virtual resume while learning how create front-end programs.
+Created using HTML, CSS, Javascript, and jQuery.
---
+++ main.js
@@ -0,0 +1,8 @@
+//initial comment wow
+function main(){
+  //alert('Hello, world!');
+  //alert(string(screen.height) + 'px');
+  //$('div').css('height', '500px');//string(screen.height) + 'px');
+}
+
+$(document).ready(main);

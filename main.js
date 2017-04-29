66b60c873fc24b1dfc268406f155b8d94b4f6550
Owen Cummings <owenpatrickcummings@gmail.com>
2017-04-29 11:40:29 -0400

    4/29 morning
    More formatting

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ function main(){
   //alert('Hello, world!');
   //alert(string(screen.height) + 'px');
   //$('div').css('height', '500px');//string(screen.height) + 'px');
+  $('#scrollButton').click(function(){
+    alert('Hello, world!');
+  })
 }
 
 $(document).ready(main);

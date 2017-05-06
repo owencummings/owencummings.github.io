f7008f7ddb428d14ab8e521a999394374c5daa9b
Owen Cummings <owenpatrickcummings@gmail.com>
2017-05-06 01:42:24 -0400

    5/5 night
    JS Plug-ins basically working

--- main.js
+++ main.js
@@ -3,92 +3,139 @@ function main(){
   //alert('Hello, world!');
   //alert(string(screen.height) + 'px');
   //$('div').css('height', '500px');//string(screen.height) + 'px');
-  setTimeout(titleInc(), 1000); //1
-  setTimeout(titleInc(), 1100); //2
-  setTimeout(titleInc(), 1200); //3
-  setTimeout(titleInc(), 1300); //4
-  setTimeout(titleInc(), 1400); //5
-  setTimeout(titleInc(), 1500); //6
-  setTimeout(titleInc(), 1600); //7
-  setTimeout(titleInc(), 1700); //8
-  setTimeout(titleInc(), 1800); //9
-  setTimeout(titleInc(), 1900); //10
-  setTimeout(titleInc(), 2000); //11
-  setTimeout(titleInc(), 2100); //12
-  setTimeout(titleInc(), 2200); //13
+  $('#audio1').click(function(){
+    if (audio1.paused){
+      audio1.currentTime = 0;
+      audio2.pause();
+      audio1.play();
+    } else {
+      audio1.pause();
+    }
+  })
+
+  $('#audio2').click(function(){
+    if (audio2.paused){
+      audio2.currentTime = 0;
+      audio1.pause();
+      audio2.play();
+    } else {
+      audio2.pause();
+    }
+  })
+
+  $('#listBut1').click(function(){
+    $('#listInfo2').slideUp(400);
+    $('#listInfo3').slideUp(400);
+    $('#listInfo4').slideUp(400);
+    $('#listInfo5').slideUp(400);
+    $('#listInfo1').slideDown(400);
+  })
+  $('#listBut2').click(function(){
+    $('#listInfo1').slideUp(400);
+    $('#listInfo3').slideUp(400);
+    $('#listInfo4').slideUp(400);
+    $('#listInfo5').slideUp(400);
+    $('#listInfo2').slideDown(400);
+  })
+  $('#listBut3').click(function(){
+    $('#listInfo2').slideUp(400);
+    $('#listInfo1').slideUp(400);
+    $('#listInfo4').slideUp(400);
+    $('#listInfo5').slideUp(400);
+    $('#listInfo3').slideDown(400);
+  })
+  $('#listBut4').click(function(){
+    $('#listInfo2').slideUp(400);
+    $('#listInfo3').slideUp(400);
+    $('#listInfo1').slideUp(400);
+    $('#listInfo5').slideUp(400);
+    $('#listInfo4').slideDown(400);
+  })
+  $('#listBut5').click(function(){
+    $('#listInfo2').slideUp(400);
+    $('#listInfo3').slideUp(400);
+    $('#listInfo4').slideUp(400);
+    $('#listInfo1').slideUp(400);
+    $('#listInfo5').slideDown(400);
+  })
+
 
 
   $('#but1').click(function(){
     $("html, body").animate({ scrollTop: "0px"}, 1500);
   })
   $('#but2').click(function(){
-    $("html, body").animate({ scrollTop: (2*(window.innerHeight)) + "px"}, 1500);
+    $("html, body").animate({ scrollTop: (2*(window.innerHeight)) + "px"}, 1000);
   })
   $('#but3').click(function(){
-    $("html, body").animate({ scrollTop: (4*(window.innerHeight)) + "px"}, 1500);
+    $("html, body").animate({ scrollTop: (4*(window.innerHeight)) + "px"}, 1000);
   })
   $('#but4').click(function(){
-    $("html, body").animate({ scrollTop: (6*(window.innerHeight)) + "px"}, 1500);
+    $("html, body").animate({ scrollTop: (6*(window.innerHeight)) + "px"}, 1000);
   })
   $('#but5').click(function(){
-    $("html, body").animate({ scrollTop: (8*(window.innerHeight)) + "px"}, 1500);
+    $("html, body").animate({ scrollTop: (8*(window.innerHeight)) + "px"}, 1000);
   })
   $('#but6').click(function(){
-    $("html, body").animate({ scrollTop: (10*(window.innerHeight)) + "px"}, 1500);
+    $("html, body").animate({ scrollTop: (10*(window.innerHeight)) + "px"}, 1000);
   })
   $('#but7').click(function(){
-    $("html, body").animate({ scrollTop: (12*(window.innerHeight)) + "px"}, 1500);
+    $("html, body").animate({ scrollTop: (12*(window.innerHeight)) + "px"}, 1000);
   })
 }
 
 var currentHeight = 0;
 var titleCounter = 0;
+var projectDepth = 2;
 var name = document.getElementById("name");
-
+var audio1 = new Audio('audio/flymetothemoon.wav');
+audio1.loop = true;
+var audio2 = new Audio('audio/groovebaby.wav');
+audio2.loop = true;
 
 function titleInc(){
   switch(titleCounter){
     case 0:
-      name.textContent = "O"
+      $('#name').text('O');
       break;
     case 1:
-      name.textContent  = "OW"
+      $('#name').text('OW');
       break;
     case 2:
-      name.textContent  = "OWE"
+      $('#name').text('OWE');
       break;
     case 3:
-      name.textContent  = "OWEN"
+      $('#name').text('OWEN');
       break;
     case 4:
-      name.textContent  = "OWEN "
+      $('#name').text('OWEN ');
       break;
     case 5:
-      name.textContent  = "OWEN C"
+      $('#name').text('OWEN C');
       break;
     case 6:
-      name.textContent  = "OWEN CU"
+      $('#name').text('OWEN CU');
       break;
     case 7:
-      name.textContent  = "OWEN CUM"
+      $('#name').text('OWEN CUM');
       break;
     case 8:
-      name.textContent  = "OWEN CUMM"
+      $('#name').text('OWEN CUMM');
       break;
     case 9:
-      name.textContent  = "OWEN CUMMI"
+      $('#name').text('OWEN CUMMI');
       break;
     case 10:
-      name.textContent  = "OWEN CUMMIN"
+      $('#name').text('OWEN CUMMIN');
       break;
     case 11:
-      name.textContent  = "OWEN CUMMING"
+      $('#name').text('OWEN CUMMING');
       break;
     case 12:
-      name.textContent  = "OWEN CUMMINGS"
+      $('#name').text('OWEN CUMMINGS');
       break;
     case 13:
-      name.textContent = "OWEN CUMMINGS."
+      $('#name').text('OWEN CUMMINGS.');
       break;
     default: break;
   }

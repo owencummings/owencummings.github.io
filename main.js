db8d95d2edc08f338987cf4dcb486cea3fdd12fa
Owen Cummings <owenpatrickcummings@gmail.com>
2017-05-17 13:33:51 -0400

    Large Visual/Plug-in Improvement
    Large improvements to visuals and JS scripts

--- main.js
+++ main.js
@@ -3,25 +3,18 @@ function main(){
   //alert('Hello, world!');
   //alert(string(screen.height) + 'px');
   //$('div').css('height', '500px');//string(screen.height) + 'px');
-  $('#audio1').click(function(){
-    if (audio1.paused){
-      audio1.currentTime = 0;
-      audio2.pause();
-      audio1.play();
-    } else {
-      audio1.pause();
-    }
-  })
+  window.onbeforeunload = function(){ window.scrollTo(0,0); }
+  currentPage = 0;
+  setTimeout(function(){
+    $('#contactBar').show("slide", {direction: "down"}, 4000);
+  }, 1000);
+  $('#but3').hide();
+  $('#but4').hide();
+  $('#but5').hide();
+  $('#but6').hide();
+  $('#but7').hide();
 
-  $('#audio2').click(function(){
-    if (audio2.paused){
-      audio2.currentTime = 0;
-      audio1.pause();
-      audio2.play();
-    } else {
-      audio2.pause();
-    }
-  })
+
 
   $('#listBut1').click(function(){
     $('#listInfo2').slideUp(400);
@@ -61,31 +54,120 @@ function main(){
 
 
 
+  $(".scrollBut").mouseenter(function(){
+    $(this).children().css('background-color', 'white');
+  })
+
+  $(".scrollBut").mouseleave(function(){
+    if (currentPage !== $('.scrollBut').index(this)) {
+      $(this).children().css('background-color', 'transparent');
+    }
+  })
+
+  $(".scrollBut").click(function(){
+    $(".scrollSquare").css('background-color', 'transparent');
+    $(this).children().css('background-color', 'white');
+    currentPage = $('.scrollBut').index(this);
+  })
+
+
   $('#but1').click(function(){
-    $("html, body").animate({ scrollTop: "0px"}, 1500);
+    $("html, body").animate({ scrollTop: "0px"}, {duration: 1000, queue: false});
+    $("#scrollBar").animate({backgroundColor: '#92cea4'}, {duration: 1000, queue: false});
+    $("#scrollBarMirror").animate({backgroundColor: '#92cea4'}, {duration: 1000, queue: false});
   })
   $('#but2').click(function(){
-    $("html, body").animate({ scrollTop: (2*(window.innerHeight)) + "px"}, 1000);
+    $("html, body").animate({ scrollTop: (2*(window.innerHeight)) + "px"}, {duration: 1000, queue:false});
+    $("#scrollBar").animate({backgroundColor: '#a189b2'}, {duration: 1000, queue: false});
+    $("#scrollBarMirror").animate({backgroundColor: '#a189b2'}, {duration: 1000, queue: false});
+    $('#but3').show("slide", {direction: "right"}, 500);
   })
   $('#but3').click(function(){
-    $("html, body").animate({ scrollTop: (4*(window.innerHeight)) + "px"}, 1000);
+    $("html, body").animate({ scrollTop: (4*(window.innerHeight)) + "px"}, {duration: 1000, queue:false});
+    $("#scrollBar").animate({backgroundColor: '#95b5c4'}, {duration: 1000, queue: false});
+    $("#scrollBarMirror").animate({backgroundColor: '#95b5c4'}, {duration: 1000, queue: false});
+    $('#but4').show("slide", {direction: "right"}, 500);
   })
   $('#but4').click(function(){
-    $("html, body").animate({ scrollTop: (6*(window.innerHeight)) + "px"}, 1000);
+    $("html, body").animate({ scrollTop: (6*(window.innerHeight)) + "px"}, {duration: 1000, queue:false});
+    $("#scrollBar").animate({backgroundColor: '#95b5c4'}, {duration: 1000, queue: false});
+    $("#scrollBarMirror").animate({backgroundColor: '#95b5c4'}, {duration: 1000, queue: false});
+    $('#but5').show("slide", {direction: "right"}, 500);
   })
   $('#but5').click(function(){
-    $("html, body").animate({ scrollTop: (8*(window.innerHeight)) + "px"}, 1000);
+    $("html, body").animate({ scrollTop: (8*(window.innerHeight)) + "px"}, {duration: 1000, queue:false});
+    $("#scrollBar").animate({backgroundColor: '#95b5c4'}, {duration: 1000, queue: false});
+    $("#scrollBarMirror").animate({backgroundColor: '#95b5c4'}, {duration: 1000, queue: false});
+    $('#but6').show("slide", {direction: "right"}, 500);
   })
   $('#but6').click(function(){
-    $("html, body").animate({ scrollTop: (10*(window.innerHeight)) + "px"}, 1000);
+    $("html, body").animate({ scrollTop: (10*(window.innerHeight)) + "px"}, {duration: 1000, queue:false});
+    $("#scrollBar").animate({backgroundColor: '#cc94b5'}, {duration: 1000, queue: false});
+    $("#scrollBarMirror").animate({backgroundColor: '#cc94b5'}, {duration: 1000, queue: false});
+    $('#but7').show("slide", {direction: "right"}, 500);
   })
   $('#but7').click(function(){
-    $("html, body").animate({ scrollTop: (12*(window.innerHeight)) + "px"}, 1000);
+    $("html, body").animate({ scrollTop: (12*(window.innerHeight)) + "px"}, {duration: 1000, queue:false});
+    $("#scrollBar").animate({backgroundColor: '#948fc1'}, {duration: 1000, queue: false});
+    $("#scrollBarMirror").animate({backgroundColor: '#948fc1'}, {duration: 1000, queue: false});
   })
+
+
+  $("#audio1").mouseenter(function(){
+    $(this).css('background-color', 'white');
+  })
+
+
+  $("#audio1").mouseleave(function(){
+    if (audio1.paused){
+      $(this).css('background-color', 'transparent');
+    }
+  })
+
+
+  $('#audio1').click(function(){
+    if (audio1.paused){
+      audio1.currentTime = 0;
+      audio2.pause();
+      $('#audio2').css('background-color', 'transparent');
+      audio1.play();
+    } else {
+      audio1.pause();
+    }
+  })
+
+  $("#audio2").mouseenter(function(){
+    $(this).css('background-color', 'white');
+  })
+
+
+  $("#audio2").mouseleave(function(){
+    if (audio2.paused){
+      $(this).css('background-color', 'transparent');
+    }
+  })
+
+  $('#audio2').click(function(){
+    if (audio2.paused){
+      audio2.currentTime = 0;
+      audio1.pause();
+      $('#audio1').css('background-color', 'transparent');
+      audio2.play();
+    } else {
+      audio2.pause();
+    }
+  })
+
+
+
 }
 
-var currentHeight = 0;
-var titleCounter = 0;
+
+
+
+
+
+var currentPage = 0;
 var projectDepth = 2;
 var name = document.getElementById("name");
 var audio1 = new Audio('audio/flymetothemoon.wav');
@@ -93,60 +175,4 @@ audio1.loop = true;
 var audio2 = new Audio('audio/groovebaby.wav');
 audio2.loop = true;
 
-function titleInc(){
-  switch(titleCounter){
-    case 0:
-      $('#name').text('O');
-      break;
-    case 1:
-      $('#name').text('OW');
-      break;
-    case 2:
-      $('#name').text('OWE');
-      break;
-    case 3:
-      $('#name').text('OWEN');
-      break;
-    case 4:
-      $('#name').text('OWEN ');
-      break;
-    case 5:
-      $('#name').text('OWEN C');
-      break;
-    case 6:
-      $('#name').text('OWEN CU');
-      break;
-    case 7:
-      $('#name').text('OWEN CUM');
-      break;
-    case 8:
-      $('#name').text('OWEN CUMM');
-      break;
-    case 9:
-      $('#name').text('OWEN CUMMI');
-      break;
-    case 10:
-      $('#name').text('OWEN CUMMIN');
-      break;
-    case 11:
-      $('#name').text('OWEN CUMMING');
-      break;
-    case 12:
-      $('#name').text('OWEN CUMMINGS');
-      break;
-    case 13:
-      $('#name').text('OWEN CUMMINGS.');
-      break;
-    default: break;
-  }
-  titleCounter = titleCounter + 1;
-}
-
-
-
-
-$(document).ready(function(){
-    $(this).scrollTop(0);
-    currentHeight = 0;
-});
 $(document).ready(main);
